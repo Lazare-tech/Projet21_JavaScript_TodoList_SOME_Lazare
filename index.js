7d83365ed34bc98f4441249e2359e6fc0deb9909
loliette <yelmaniyel@gmail.com>
2023-08-12 19:54:45 +0000

    ajout supression et modification d'une todo list

--- index.js
+++ index.js
@@ -1,79 +1,78 @@
-const input_box = document.getElementById("input_box");
-const liste = document.querySelector(".list-group");
-const att = document.createAttribute("class")
-const add = document.getElementById("add")
-const list_content=document.getElementById("list-container")
- //creation de la balise li
 
-  
-  //ajout de li dans list_content
+//on stocker la valeur saisie dans input_box
+let input_box = document.getElementById("input_box"),
+//selection des lis dans se trouvant list-container
+    items = document.querySelectorAll("#list-container li"),
+    //declaration dun tableau tab et variable index
+    tab = [], index;
+//boucle for 
+for (let i = 0; i < items.length; i++) {
+    //insertion d la valeur de input dans le tableau tab avec son rang
+    tab.push(items[i].innerHTML);
     
-  let li,i,check;
-  //ajout de classe a la balise
- 
-  //
-//fonction
-    add.addEventListener("click", function (e) {
+}
 
+//boucle for lorsquon click sur un li
+for (let i = 0; i < items.length; i++) {
+    //lors du click sur un li specifique,une fonction sexecute
+    items[i].onclick = function () {
+        //et undex contient la valeur du li et son rang
+        index = tab.indexOf(this.innerHTML);
+        //lors du clic sur un li, on le met dans le input
+        input_box.value = this.innerHTML
 
-    if(input_box.value === ""){
-        alert("Vous devez ecrire une tache")
-    }else {
-       
-        li= document.createElement("li");
-        i=document.createElement("i")
-        check=document.createElement("input")
+    }
+}
+//mis a jour du tableau 
 
-        //ajout de la valeur de input dans le li
-        li.innerHTML = input_box.value;
-         // let div=document.createElement("div")
-         li.setAttribute("class","list-group-item flex-item-left")
-         li.setAttribute("onclick","f(this)")
-         i.setAttribute("class","fas fa-times")
-         //  let span =document.createElement("span")
-         // span.innerHTML= "\u00d7";
-         //ajout du span dans li
-         check.setAttribute("class","form-check-input")
-         check.setAttribute("type","checkbox")
-       
-         list_content.appendChild(li);
+function mettreAJourTableau() {
+ //on met la taille du tableau a zero
+    tab.length = 0;
+    items = document.querySelectorAll("#list-container li")
+    for (let i = 0; i < items.length; i++) {
+        tab.push(items[i].innerHTML);
+    }
+}
+function add() {
+    //selection de lid list-container
+    let list_content = document.getElementById("list-container"),
+    //ajout de la valeur saisie dans input dans text
+        text = document.createTextNode(input_box.value),
+        //creation de li
+        li = document.createElement("LI");
+    //ajout du text saisie dans li
+    li.appendChild(text);
+    //et ajout du li dans list_content 
+    list_content.appendChild(li)
+    //appel de la fonction mettreAjourTableau pour ajouter la valeur saisie dans le input dans le tableau tab
+    mettreAJourTableau();
+    // lors du click sur un li 
+    li.onclick = function () {
+        //on recupere son rang
+        index = tab.indexOf(li.innerHTML);
+        //log console pour afficher le rang
+        // console.log(li.innerHTML + "INDEX" + index);
+        //lors du clic sur un li, on le met dans le input
+        input_box.value = li.innerHTML
 
-        li.prepend(check)
-        li.appendChild(i)
-        console.log(li);
     }
 
-    input_box.value = "";
-    // save();
+}
+//fonction editer
+function edit() {
+    //editer le li selectionner et present dans le input
+    items[index].innerHTML = input_box.value;
+    //appel de la fonction mettreAjourTableau pour enregistrer les modifications
+    mettreAJourTableau();
 
-})
-function f(e){
+}
+//fonction de supression
+function delet() {
+        //appel de la fonction mettreAjourTableau
+    mettreAJourTableau();
+    if (items.length > 0) {
+        items[index].parentNode.removeChild(items[index]);
+        input_box.value = ""
 
-li.style.textDecoration="line-through"
-li.remove(e)
-   }
-
-    list_content.addEventListener("click",function(e){
-        
-        if(e.target.tagName === "LI"){
-            e.target.classList.toggle("checked");
-            
-            // save()
-        }
-        else if(e.target.tagName === "span"){
-            e.target.parentElement.remove();
-            // save()
-        }
-    }, false);
-    //
- 
-//  function save(){
-//      localStorage.setItem("data", list_content.innerHTML);
-//  }
-
-//  function showList(){
-//      list_content.innerHTML= localStorage.getItem("data")
-//  }
-//  showList()
-// };
-// ajouterTache()
+    }
+}
